d52dff7171bd0762ce777ef6651520d095f9035e
puneetmanghwani <puneet.manghwani@axestrack.com>
2021-03-09 18:31:15 +0530

    server side with loggin started

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
+const pino = require('pino');
+const expressPino = require('express-pino-logger');
 const urlRoutes = require('./routes/urlShorten');
-const constants = require('./configs/constants');
+
+const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
+const expressLogger = expressPino({ logger });
 
 const app = express();
 app.use(cors());
-
+app.use(expressLogger);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -17,12 +23,14 @@ app.use(cookieParser());
 
 app.use('/api', urlRoutes);
 
+const { mongoURI, serverPort } = process.env;
+logger.info('Running on %d', serverPort);
 mongoose
   .connect(
-    constants.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true },
+    mongoURI, { useNewUrlParser: true, useUnifiedTopology: true },
   )
   .then(() => {
-    app.listen(process.env.PORT || 8000);
+    app.listen(serverPort);
   })
   .catch((err) => {
     console.log(err);
--- configs/constants.js
+++ configs/constants.js
@@ -1,3 +1,4 @@
 module.exports = {
   mongoURI: 'mongodb+srv://puneet:puneet@cluster0.adp1h.mongodb.net/url_shortner?retryWrites=true&w=majority',
+  serverPort: 8000,
 };

d0812969006f345c11b5c07e5596550f95ad5386
puneetmanghwani <puneet.manghwani@axestrack.com>
2021-03-12 00:26:18 +0530

    localhost change

--- client/src/components/urlShortner.js
+++ client/src/components/urlShortner.js
@@ -29,7 +29,7 @@ function UrlShortner(props) {
         api
         .generateShortUrl(formData)
         .then(( { data } ) => {
-            const shortUrl = 'http://localhost/'+data.urlData.urlCode;
+            const shortUrl = 'http://52.170.145.190/'+data.urlData.urlCode;
             setState({
                 ...state,
                 shortUrl,
